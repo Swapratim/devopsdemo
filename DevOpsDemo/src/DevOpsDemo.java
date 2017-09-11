c242f8cbb70ca56af7478531ac5e8df793c7b3fa
m48122 <m48122@W7B00289.tdk.dk>
2017-09-11 22:21:49 +0200

    DevOps Demo - Version 2.0
    Change-Id: I0ee2feace0f6f0a7c394c591b82f2e6e6a11496e

--- DevOpsDemo/src/DevOpsDemo.java
+++ DevOpsDemo/src/DevOpsDemo.java
@@ -1,7 +1,7 @@
 
 public class DevOpsDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello Viewers...Welcome to the Demo of DevOps");
+		System.out.println("Hello Viewers...Welcome to the Demo of DevOps!!!");
 	}
 
 }
